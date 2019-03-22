40bf39d84e9eb29fc993a0ae900ac628749a310b
NanJiang327 <nj499521010@gmail.com>
2019-03-22 22:39:48 +1300

    Remove files

--- movie-in-one/src/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  douban: {
-    basicUrl: 'https://api.douban.com/v2/movie/',
-  },
-  tmdb: {
-    basicUrl: 'https://api.themoviedb.org/3/movie/',
-    apiKey: 'bb3f5acf00144cfacd624c940807a5c4'
-  }
-}
