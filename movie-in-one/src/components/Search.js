e4ba3c703e97906a6c510033d12b051739700401
NanJiang327 <nj499521010@gmail.com>
2019-03-24 01:01:55 +1300

    Finsish Search

--- movie-in-one/src/components/Search.js
+++ movie-in-one/src/components/Search.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import IconFont from './IconFont'
+import debounce from 'lodash.debounce'
+import axios from 'axios'
+import { connect } from 'react-redux';
+import config from '../utils/config'
 
 class Search extends Component {
-  state = { focused: false }
+  constructor() {
+    super()
+    this.fetchData = debounce(this.fetchData, 300)
+    this.state = { focused: false, searchResult: [], showSearchMenu: false}
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', ()=> {this.setState({ showSearchMenu: false})})
+  }
+
+  fetchData = (value) => {
+    const api = config.tmdb.searchUrl + '?query='+ value +'&api_key=' + config.tmdb.apiKey + '&language=' + this.props.language +'&page=1'
+    if (!value) return this.setState({ showSearchMenu: false })
+    this.setState({ showSearchMenu: true })
+    axios.get(api).
+      then((res) => {
+        this.setState({ searchResult: res.data.results})
+      }).catch(err => {
+        console.log(err)
+      })
+  }
 
   onKeyDown = e => {
     if (e.keyCode === 13) {
@@ -12,6 +36,17 @@ class Search extends Component {
       this.props.history.push(path);
     }
   };
+
+  handleInput = (e) => {
+    this.fetchData(e.target.value)
+  }
+
+  handleClick = (e) => {
+    this.refs.input.value = ''
+    this.setState({
+      searchResult:[]
+    })
+  }
 
 
   render () {
@@ -29,13 +64,40 @@ class Search extends Component {
           ref="input"
           type="text"
           onKeyDown={this.onKeyDown}
+          onChange={this.handleInput}
           onFocus={() => this.setState({ focused: true })}
           onBlur={() => this.setState({ focused: false })}
           placeholder="Search for a movie..."
         />
+        {
+          (this.state.showSearchMenu && this.state.searchResult.length >= 0)? 
+          <ul className="dropdown-menu ">
+            {
+              this.state.searchResult.length !== 0 ?
+                this.state.searchResult.map((movie, index) => {
+                  return index <= 5 ? (
+                    <li key={movie.id} className="suggestion" onClick={this.handleClick}>
+                      <Link to={'/detail/' + movie.id}>
+                        {movie.title}
+                      </Link>
+                    </li>
+                  ) : null
+                }) : <li>No search result</li>
+            }
+          </ul> : null
+        }
       </div>
     )
   }
 }
 
-export default withRouter(Search);
+function mapStateToProps(state) {
+  return {
+    language: state.language
+  }
+}
+
+
+export default connect(
+  mapStateToProps
+)(withRouter(Search));
