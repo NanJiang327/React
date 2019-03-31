7c736b63b6c65189d271d053edec66a176482778
NanJiang327 <nj499521010@gmail.com>
2019-03-31 14:37:15 +1300

    Finish review component

--- movie-in-one/src/components/Review.js
+++ movie-in-one/src/components/Review.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Loading from './Loading'
 import axios from 'axios'
+import {
+  Comment, List, Avatar
+} from 'antd';
 
 import config from '../utils/config'
 
@@ -39,16 +42,32 @@ class Review extends Component {
      })
   }
 
+  
+
   render () {
     if (!this.state.ready) return <Loading />
     return (
       <div>
         {
-          this.state.reviews.map((review, idx) => {
-            return (
-              <div>Hello World</div>
-            )
-          })
+          <List
+            className="comment-list"
+            header={'Reviews'}
+            itemLayout="horizontal"
+            dataSource={this.state.reviews}
+            renderItem={item => (
+              <Comment
+                author={<a href={item.url}>{item.author}</a>}
+                content={(
+                  <p>{item.content}</p>
+                )}
+                avatar={(
+                <Avatar
+                  src="https://pic2.zhimg.com/80/v2-1e02c1531c33f9460ae82eb88a999cdd_hd.jpg"
+                />
+                 )}
+              />
+            )}
+          /> 
         }
       </div>
     )
