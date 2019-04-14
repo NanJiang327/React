95ab20c061b56c80a5bfa0705e6d5f4b2b062d20
NanJiang327 <nj499521010@gmail.com>
2019-04-14 18:52:44 +1200

    finish 2nd course - stars guessing game

---
+++ e1-02/src/PlayNumber.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+
+export default function PlayNumber (props) {
+  return (
+    <button
+    className="number"
+    style={{backgroundColor: colors[props.status]}}
+    onClick={() => props.onClick(props.number, props.status)}
+    >
+      {props.number}
+    </button>
+  )
+}
